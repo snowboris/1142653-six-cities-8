c92d86805b43ba049c693dd937ea552016d5c244
Boris Olenev <olenevboris95@yandex.ru>
2021-10-25 22:09:40 +0700

    Add pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,45 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import React from 'react';
+import MainPage from '../main/main';
+
+type Location = {
+  latitude: number,
+  longitude: number,
+  zoom: number
+}
+
+type Hotel = {
+  bedrooms: number,
+  city: {
+    location: Location,
+    name: string
+  }
+  description: string,
+  goods: string[],
+  host: {
+    avatarUrl: string,
+    id: number,
+    isPro: boolean,
+    name: string
+  }
+  id: number,
+  images: string[],
+  isFavorite: boolean,
+  isPremium: boolean,
+  location: Location,
+  maxAdults: number,
+  previewImage: string,
+  price: number,
+  rating: number,
+  title: string,
+  type: string
+}
+
+type AppProps = {
+  data: Hotel[]
+}
+
+function App({ data }: AppProps): JSX.Element {
+  return <MainPage data={data} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,214 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+type Location = {
+  latitude: number,
+  longitude: number,
+  zoom: number
+}
+
+type Hotel = {
+  bedrooms: number,
+  city: {
+    location: Location,
+    name: string
+  }
+  description: string,
+  goods: string[],
+  host: {
+    avatarUrl: string,
+    id: number,
+    isPro: boolean,
+    name: string
+  }
+  id: number,
+  images: string[],
+  isFavorite: boolean,
+  isPremium: boolean,
+  location: Location,
+  maxAdults: number,
+  previewImage: string,
+  price: number,
+  rating: number,
+  title: string,
+  type: string
+}
+
+const data: Hotel[] = [
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 1,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': true,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/apartment-01.jpg',
+    'price': 120,
+    'rating': 4.8,
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment',
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 2,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/room.jpg',
+    'price': 80,
+    'rating': 4,
+    'title': 'Wood and stone place',
+    'type': 'Private room',
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 3,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/apartment-02.jpg',
+    'price': 132,
+    'rating': 4,
+    'title': 'Canal View Prinsengracht',
+    'type': 'Apartment',
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 4,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': true,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/apartment-03.jpg',
+    'price': 180,
+    'rating': 5,
+    'title': 'Nice, cozy, warm big bed apartment',
+    'type': 'Apartment',
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': 'Amsterdam',
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina',
+    },
+    'id': 5,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'maxAdults': 4,
+    'previewImage': 'img/room.jpg',
+    'price': 180,
+    'rating': 4,
+    'title': 'Wood and stone place',
+    'type': 'Private room',
+  },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>,
   document.getElementById('root'));
